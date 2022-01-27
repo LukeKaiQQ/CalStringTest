163236a3de524943d4520ead1a6e10c7bdfaf0b6
LukeKaiQQ <https://github.com/LukeKaiQQ>
2022-01-27 22:18:11 +0800

    Commit Calculate

--- src/main/java/tw/com/fcb/calstringtest/CalculateString/CalculateStringApp.java
+++ src/main/java/tw/com/fcb/calstringtest/CalculateString/CalculateStringApp.java
@@ -3,8 +3,6 @@ package tw.com.fcb.calstringtest.CalculateString;
 public class CalculateStringApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		// input option...
 		CalculateStringService service = new CalculateStringService();
 		service.readOption();
